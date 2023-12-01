4cb9d052c1f4e29f75f1c7befa5594d20a133803
Madhav <Madhavampire@gmail.com>
2023-12-01 19:39:01 +0530

    /users_info

--- keep_alive.py
+++ keep_alive.py
@@ -1,7 +1,8 @@
 # keep alive server => uptimerobot
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from threading import Thread
+import json
 
 app = Flask('')
 
@@ -14,14 +15,26 @@ def logs():
     # display logs
     try:
         with open('.logs.txt', 'r') as file:
-            display = file.read().strip()
+            data = file.read().strip()
     except Exception as err:
-        display = f"[error] {str(err)}"
+        data = f"[error] {str(err)}"
 
     return render_template(
         'logs.html', 
-        content = display
+        content = data
     )
+
+@app.route('/users_info')
+def users_info():
+    try:
+        with open('.users.json', 'r') as json_file:
+            data = json.load(json_file)
+            prettified_data = json.dumps(data, indent=4)
+        return jsonify(prettified_data)
+    except FileNotFoundError:
+        return "File not found", 404
+    except Exception as err:
+        return f"[error] {str(err)}", 500
 
 def run():
     app.run(host='0.0.0.0', port=8000)
